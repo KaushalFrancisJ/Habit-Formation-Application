42305dbdbd0d0130377c1391d6c5755d23dac90c
Kaushal Francis J <jkfrancis2003@gmail.com>
2026-06-20 16:23:06 +0530

    production deployment fixes

--- db/config.cjs
+++ db/config.cjs
@@ -11,4 +11,14 @@ module.exports = {
     migrationStorageTableName: 'sequelize_migrations',
     seederStorageTableName: 'sequelize_seeders',
   },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '3306', 10),
+    dialect: 'mysql',
+    migrationStorageTableName: 'sequelize_migrations',
+    seederStorageTableName: 'sequelize_seeders',
+  },
 };
